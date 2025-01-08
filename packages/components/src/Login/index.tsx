ce30540b1a95fce5603a196e4cab19bf8b091384
siyeon00(이시연) <siyeon00@palnet.co.kr>
2025-01-08 14:35:10 +0900

    feat : Defualt 오류 수정 (export interface)

--- packages/components/src/Login/index.tsx
+++ packages/components/src/Login/index.tsx
@@ -2,25 +2,29 @@ import React, { useState } from "react";
 import "./Login.css";
 import Typography from "../Typography";
 import Input from "../input";
+import Button from "../button";
 
-interface LoginProps {
+export interface LoginFormProps
+  extends Omit<React.HTMLAttributes<HTMLFormElement>, "onSubmit"> {
   onSubmit?: (data: { id: string; pw: string }) => void;
 }
-
-export const Login: React.FC<LoginProps> = ({ onSubmit }) => {
+export const Login: React.FC<LoginFormProps> = ({ onSubmit }) => {
   const [id, setId] = useState("");
   const [pw, setPw] = useState("");
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
-    alert(`아이디: ${id}, 비밀번호: ${pw}`);
-    if (onSubmit) {
+
+    if (id === "" && pw === "") {
+      alert("아이디 또는 비밀번호를 입력해 주세요");
+    } else if (onSubmit) {
       onSubmit({ id, pw });
+      alert(`아이디: ${id}, 비밀번호: ${pw}`);
     }
   };
 
   return (
-    <form className="login_form">
+    <form className="login_form" onSubmit={handleLogin}>
       <Typography variant="h2" color="default" align="center" bold>
         로그인
       </Typography>
@@ -39,12 +43,16 @@ export const Login: React.FC<LoginProps> = ({ onSubmit }) => {
           setPw(e.target.value);
         }}
       />
-      <button type="submit" className="login_button" onClick={handleLogin}>
+      <Button
+        type="submit"
+        className="login_button"
+        variant="primary"
+        size="medium"
+      >
         로그인
-      </button>
+      </Button>
     </form>
   );
 };
 
-Login.displayName = "Login";
 export default Login;
--- packages/storybook/src/designSystem/Login.stories.tsx
+++ packages/storybook/src/designSystem/Login.stories.tsx
@@ -6,13 +6,18 @@ export default {
   title: "Components/Login",
   component: Login,
   tags: ["autodocs"],
-  argTypes: {},
+  argTypes: {
+    onSubmit: {
+      action: "onSubmit",
+      description: "폼 제출 시 호출되는 콜백 함수",
+    },
+  },
 } as Meta<typeof Login>;
 
 const Template: Story<typeof Login> = (args) => <Login {...args} />;
 
-export const DefaultStory = Template.bind({});
-DefaultStory.args = {
+export const Default = Template.bind({});
+Default.args = {
   onSubmit: (data: { id: string; pw: string }) => {
     console.log("로그인 요청 데이터:", data);
   },
