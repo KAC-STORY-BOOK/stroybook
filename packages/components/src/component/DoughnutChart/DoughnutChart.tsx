2468224da2544e7b7f6c847824f852a9a723fa32
sanguu516 <sanguu516@naver.com>
2025-01-14 14:36:56 +0900

    cart 라이브러리 추가

---
+++ packages/components/src/component/DoughnutChart/DoughnutChart.tsx
@@ -0,0 +1,38 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+
+// Chart.js/auto가 필요할 경우 추가 설정
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+interface DoughnutChartProps {
+  labels?: string[];
+  data?: number[];
+  backgroundColors?: string[];
+  borderColors?: string[];
+  borderWidth?: number;
+}
+
+const DoughnutChart: React.FC<DoughnutChartProps> = ({
+  labels,
+  data,
+  backgroundColors,
+  borderColors,
+  borderWidth,
+}) => {
+  const chartData = {
+    labels,
+    datasets: [
+      {
+        data,
+        backgroundColor: backgroundColors,
+        borderColor: borderColors,
+        borderWidth: borderWidth,
+      },
+    ],
+  };
+
+  return <Doughnut data={chartData} />;
+};
+
+export default DoughnutChart;
